44f6be9ca08cce6d7b9a6618c532a4e6a89f1aff
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-24 16:05:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import calendar
-from chart_page import render_chart_page
 
 st.set_page_config(page_title="Inventory Analysis (Admin)", layout="wide")
 st.title("💹 Inventory Analysis")
@@ -92,4 +91,3 @@ if df_raw.empty:
 
 # 👉 Save to session for Chart page
 st.session_state["official_data"] = df_raw
-render_chart_page()
