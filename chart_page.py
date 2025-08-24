007e39553a2e39086b9d6c00fb8300c32c9fcd1e
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-24 20:35:24 +0700

    Update chart_page.py

--- chart_page.py
+++ chart_page.py
@@ -34,7 +34,7 @@ def render_chart_page():
     chart_df = (
         df_filtered.groupby(["Item Code", "Operation Date", "Rcv So Flag"], as_index=False)["Quantity[Unit1]"]
         .sum()
-        .sort_values("Operation Date")
+        .sort_values("Item Code")
     )
 
     # --- Line Chart ---
