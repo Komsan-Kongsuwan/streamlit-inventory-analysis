a33dbd1b3c892bbb30245af44cbfc5eeaefec459
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-25 10:13:35 +0700

    Update chart_page.py

--- chart_page.py
+++ chart_page.py
@@ -61,9 +61,6 @@ def render_chart_page():
     if items:
         df_filtered = df_filtered[df_filtered["Item Code"].isin(items)]
 
-    # --- Keep only Rcv(increase) ---
-    df_filtered = df_filtered[df_filtered["Rcv So Flag"] == "Rcv(increase)"]
-
     if df_filtered.empty:
         st.warning("⚠️ No data after filtering.")
         return
