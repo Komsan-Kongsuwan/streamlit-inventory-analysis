91605f46b934c5dcb06932200f739ab6a325a60c
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-26 13:04:52 +0700

    Update chart_page.py

--- chart_page.py
+++ chart_page.py
@@ -34,7 +34,7 @@ def render_chart_page():
     """, unsafe_allow_html=True)
 
     # --- Page Title ---
-    st.markdown("<h2 style='text-align:left; font-size:28px;'>📊 Receive-Ship Visualization</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align:left; font-size:28px;'>📊 Receive-Ship Visualization (3)</h2>", unsafe_allow_html=True)
 
     if "official_data" not in st.session_state:
         st.warning("⚠️ No data found. Please upload files in the Data Loader page first.")
